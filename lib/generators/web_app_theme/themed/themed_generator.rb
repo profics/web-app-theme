dcac2a882c6e7e15e9fa42374be6992f0560b00e
github@profics.ch <ls@feanor.profics.local>
2012-09-12 14:44:49 +0200

    Replace will_paginate with paginate (for Kaminari).

--- lib/generators/web_app_theme/themed/themed_generator.rb
+++ lib/generators/web_app_theme/themed/themed_generator.rb
@@ -10,7 +10,7 @@ module WebAppTheme
     
     class_option :layout,         :type => :string,   :desc => 'Specify the layout name'
     class_option :engine,         :type => :string,   :default => 'erb', :desc => 'Specify the template engine'
-    class_option :will_paginate,  :type => :boolean,  :default => false, :desc => 'Specify if you use will_paginate'
+    class_option :paginate,  :type => :boolean,  :default => false, :desc => 'Specify if you use paginate'
     class_option :themed_type,    :type => :string,   :default => 'crud', :desc => 'Specify the themed type, crud or text. Default is crud'
     
     def initialize(args, *options)
