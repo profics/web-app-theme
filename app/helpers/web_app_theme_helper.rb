59f91b33cb296b1e30e05f1411594953c2d9da43
frankvollenweider <frank@vollenweider.info>
2012-09-19 14:31:18 +0200

    enable simple_form_style

---
+++ app/helpers/web_app_theme_helper.rb
@@ -0,0 +1,45 @@
+module WebAppThemeHelper
+
+  def model_klass
+    controller.model_klass
+  end
+  
+  def t_attr(type_string = nil, symbol)
+    type_string ||= controller.model_type
+    t(type_string.underscore.gsub(/^/, 'activerecord.attributes.').gsub(/$/, ".#{symbol}"), :default => symbol.to_s.humanize)
+  end
+  
+  def t_klass(type_string = nil, count = 1)
+    type_string ||= controller.model_type
+    t(type_string.underscore.gsub(/^/, 'activerecord.models.'), :count => count, :default => type_string.humanize)
+  end
+  
+  def t_klasses(type_string = nil)
+    t_klass(type_string, 2)
+  end
+  
+  def t_wat(string_or_symbol)
+    opts = {
+      :scope => 'web-app-theme', 
+      :default => string_or_symbol.to_s.titleize
+    }
+    t(string_or_symbol, opts)
+  end
+  
+  def t_nav1(string_or_symbol)
+    opts = {
+      :scope => ["web-app-theme.nav", string_or_symbol.to_s].join("."),
+      :default => string_or_symbol.to_s.titleize
+    }
+    t(:label, opts)
+  end
+  
+  def t_nav2(first_level, string_or_symbol)
+    opts = {
+      :scope => ["web-app-theme.nav", first_level.to_s].join("."),
+      :default => string_or_symbol.to_s.titleize
+    }
+    t(string_or_symbol, opts)
+  end
+
+end
--- lib/generators/web_app_theme/assets/assets_generator.rb
+++ lib/generators/web_app_theme/assets/assets_generator.rb
@@ -1,16 +1,20 @@
 module WebAppTheme
     class AssetsGenerator < Rails::Generators::Base
       desc "Copy the --theme stylesheets to your rails application assets path, so you can customize them"
-      source_root File.expand_path('../../../../../app/assets/', __FILE__)
+      source_root File.expand_path('../../../../../app/', __FILE__)
       class_option :theme,        :type => :string,   :default => :default,   :desc => 'Specify the layout theme to be copied'
 
       def copy_stylesheets
-        copy_file "stylesheets/web-app-theme/base.css"                , "app/assets/stylesheets/web-app-theme/base.css"
-        directory "stylesheets/web-app-theme/themes/#{options.theme}" , "app/assets/stylesheets/web-app-theme/themes/#{options.theme}"
+        copy_file "assets/stylesheets/web-app-theme/base.css"                , "app/assets/stylesheets/web-app-theme/base.css"
+        directory "assets/stylesheets/web-app-theme/themes/#{options.theme}" , "app/assets/stylesheets/web-app-theme/themes/#{options.theme}"
       end
 
       def copy_images
-        directory "images/web-app-theme/themes/#{options.theme}"      , "app/assets/images/web-app-theme/themes/#{options.theme}"
+        directory "assets/images/web-app-theme/themes/#{options.theme}"      , "app/assets/images/web-app-theme/themes/#{options.theme}"
+      end
+
+      def copy_helper
+        copy_file "helpers/web_app_theme_helper.rb", "app/helpers/web_app_theme_helper.rb"
       end
 
     end
