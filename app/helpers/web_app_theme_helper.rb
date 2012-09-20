5a611597dacc51bd934ef99c91a207f303913bad
frankvollenweider <frank@vollenweider.info>
2012-09-20 12:56:02 +0200

    using css for pictures

--- app/helpers/web_app_theme_helper.rb
+++ app/helpers/web_app_theme_helper.rb
@@ -1,16 +1,20 @@
 module WebAppThemeHelper
 
-  def model_klass
-    controller.class.to_s.gsub(/Controller/, '').singularize.constantize
+  def model_type
+    controller.class.to_s.gsub(/Controller/, '').singularize
   end
-  
-  def t_attr(type_string = nil, symbol)
-    type_string ||= controller.model_type
-    t(type_string.underscore.gsub(/^/, 'activerecord.attributes.').gsub(/$/, ".#{symbol}"), :default => symbol.to_s.humanize)
+
+  def model_klass
+    model_type.constantize
+  end
+
+  def t_attr(type_string = nil, string_or_symbol)
+    type_string ||= model_type
+    t(type_string.underscore.gsub(/^/, 'activerecord.attributes.').gsub(/$/, ".#{string_or_symbol}"), :default => t("activerecord.labels.#{string_or_symbol}", :default => string_or_symbol.to_s.humanize))
   end
   
   def t_klass(type_string = nil, count = 1)
-    type_string ||= controller.model_type
+    type_string ||= model_type
     t(type_string.underscore.gsub(/^/, 'activerecord.models.'), :count => count, :default => type_string.humanize)
   end
   
